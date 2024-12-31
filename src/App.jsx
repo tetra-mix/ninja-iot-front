808be16587ce91c38406235dcb8179f30bdc2f5f
tetra-mix <ryouma1128@outlook.jp>
2024-12-31 19:01:06 +0900

    update: system

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
 import { useState, useLayoutEffect } from 'react'
 import { Container, Button, useColorMode, } from "@yamada-ui/react"
 import { GPSContent } from './contents/GPSContent';
+import { Top } from './contents/Top';
 import { LoadView } from './atom/LoadView';
 import { Header } from './atom/Header';
 import { Footer } from './atom/Footer';
 
 function App() {
-  const [flag, setFlag] = useState(0);
   const { colorMode, changeColorMode, toggleColorMode } = useColorMode()
+  const [flag, setFlag] = useState(0);
 
-  // ビジーwaitを使う方法
-  const sleep = (waitMsec) => {
-    var startMsec = new Date();
-
-    // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
-    while (new Date() - startMsec < waitMsec);
+  const loadingEvent = () => {
+    setFlag(1);
+    window.setTimeout(() => { setFlag(2); }, 2000);
   }
 
   useLayoutEffect(() => {
     changeColorMode("dark");
   }, []);
-  const loadingEvent = () => {
-    setFlag(1);
-    window.setTimeout(()=>{setFlag(2);}, 2000);
-  }
+
   return (
     <>
       <Header />
       <Container fontFamily={"DotGothic16"} >
         {
-          flag == 0 ? <Button mt="32" colorScheme={"secondary"} onClick={loadingEvent}>探知開始</Button> : <></>
+          flag == 0 ? <Top><Button mt="4" colorScheme={"secondary"} onClick={loadingEvent}>探知開始</Button></Top> : <></>
         }
         {
           flag == 1 ? <LoadView /> : <></>
@@ -38,7 +33,7 @@ function App() {
           flag == 2 ? <GPSContent /> : <></>
         }
       </Container>
-      <Footer/>
+      <Footer />
     </>
   )
 }
--- src/contents/GPSContent.jsx
+++ src/contents/GPSContent.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect, useRef } from "react"
 import { Box, Flex, Heading, Text, Button, Center } from "@yamada-ui/react"
 import { RelayServer } from "https://chirimen.org/remote-connection/js/beta/RelayServer.js";
-import { gsap } from 'gsap'
-import { TextPlugin } from 'gsap/TextPlugin'
 
 
 export const GPSContent = () => {
@@ -47,8 +45,12 @@ export const GPSContent = () => {
             setDetectColor("primary.200");
         }
 
+        // クリップボードにあるbase64文字列を貼り付けます
+        var base64 = "UklGRnoGAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQoGAACBhYqFbF1fdJivrJBhNjVgodDbq2EcBj+a2/LDciUFLIHO8tiJNwgZaLvt559NEAxQp+PwtmMcBjiR1/LMeSwFJHfH8N2QQAoUXrTp66hVFApGn+DyvmwhBTGH0fPTgjMGHm7A7+OZSA0PVqzn77BdGAg+ltryxnMpBSl+zPLaizsIGGS57OihUBELTKXh8bllHgU2jdXzzn0vBSF1xe/glEILElyx6OyrWBUIQ5zd8sFuJAUuhM/z1YU2Bhxqvu7mnEoODlOq5O+zYBoGPJPY88p2KwUme8rx3I4+CRZiturqpVITC0mi4PK8aB8GM4nU8tGAMQYfcsLu45ZFDBFYr+ftrVoXCECY3PLEcSYELIHO8diJOQcZaLvt559NEAxPqOPwtmMcBjiP1/PMeS0GI3fH8N2RQAoUXrTp66hVFApGnt/yvmwhBTCG0fPTgjQGHW/A7eSaRw0PVqzl77BeGQc9ltvyxnUoBSh+zPDaizsIGGS56+mjTxELTKXh8bllHgU1jdT0z3wvBSJ0xe/glEILElyx6OyrWRUIRJve8sFuJAUug8/y1oU2Bhxqvu3mnEoPDlOq5O+zYRsGPJLZ88p3KgUme8rx3I4+CRVht+rqpVMSC0mh4fK8aiAFM4nU8tGAMQYfccPu45ZFDBFYr+ftrVwWCECY3PLEcSYGK4DN8tiIOQcZZ7zs56BODwxPpuPxtmQcBjiP1/PMeywGI3fH8N+RQAoUXrTp66hWEwlGnt/yv2wiBDCG0fPTgzQHHG/A7eSaSQ0PVqvm77BeGQc9ltrzxnUoBSh9y/HajDsIF2W56+mjUREKTKPi8blnHgU1jdTy0HwvBSF0xPDglEQKElux6eyrWRUJQ5vd88FwJAQug8/y1oY2Bhxqvu3mnEwODVKp5e+zYRsGOpPX88p3KgUmecnw3Y4/CBVhtuvqpVMSC0mh4PG9aiAFM4nS89GAMQYfccLv45dGCxFYrufur1sYB0CY3PLEcycFKoDN8tiIOQcZZ7rs56BODwxPpuPxtmQdBTiP1/PMey4FI3bH8d+RQQkUXbPq66hWFQlGnt/yv2wiBDCG0PPTgzUGHG3A7uSaSQ0PVKzm7rJeGAc9ltrzyHQpBSh9y/HajDwIF2S46+mjUREKTKPi8blnHwU1jdTy0H4wBiF0xPDglEQKElux5+2sWBUJQ5vd88NvJAUtg87y1oY3Bxtpve3mnUsODlKp5PC1YRsHOpHY88p3LAUlecnw3Y8+CBZhtuvqpVMSC0mh4PG9aiAFMojT89GBMgUfccLv45dGDRBYrufur1sYB0CX2/PEcycFKoDN8tiKOQgZZ7vs56BOEQxPpuPxt2MdBTeP1vTNei4FI3bH79+RQQsUXbTo7KlXFAlFnd7zv2wiBDCF0fLUgzUGHG3A7uSaSQ0PVKzm7rJfGQc9lNrzyHUpBCh9y/HajDwJFmS46+mjUhEKTKLh8btmHwU1i9Xyz34wBiFzxfDglUMMEVux5+2sWhYIQprd88NvJAUsgs/y1oY3Bxpqve3mnUsODlKp5PC1YhsGOpHY88p5KwUlecnw3Y8+ChVgtunqp1QTCkig4PG9ayEEMojT89GBMgUfb8Lv4pdGDRBXr+fur1wXB0CX2/PEcycFKn/M8diKOQgZZrvs56BPEAxOpePxt2UcBzaP1vLOfC0FJHbH79+RQQsUXbTo7KlXFAlFnd7xwG4jBS+F0fLUhDQGHG3A7uSbSg0PVKrl7rJfGQc9lNn0yHUpBCh7yvLajTsJFmS46umkUREMSqPh8btoHgY0i9Tz0H4wBiFzw+/hlUULEVqw6O2sWhYIQprc88NxJQUsgs/y1oY3BxpqvO7mnUwPDVKo5PC1YhsGOpHY8sp5KwUleMjx3Y9ACRVgterqp1QTCkig3/K+aiEGMYjS89GBMgceb8Hu45lHDBBXrebvr1wYBz+Y2/PGcigEKn/M8dqJOwgZZrrs6KFOEAxOpd/js2coGUCLydq6e0MlP3uwybiNWDhEa5yztJRrS0lnjKOkk3leWGeAlZePfHRpbH2JhoJ+fXl9TElTVEQAAABJTkZPSUNSRAsAAAAyMDAxLTAxLTIzAABJRU5HCwAAAFRlZCBCcm9va3MAAElTRlQQAAAAU291bmQgRm9yZ2UgNC41AA=="
+        // datauri scheme 形式にして Audio オブジェクトを生成します
+        var sound = new Audio("data:audio/wav;base64," + base64);
+        sound.play();
         navigator.vibrate(200);
-        setAnimation();
     }
 
     const FloatToString = (num) => {
@@ -88,21 +90,8 @@ export const GPSContent = () => {
         );
     };
 
-    const setAnimation = () => {
-        gsap.to(".detect", {
-            duration: 2, //アニメーション時間（秒）
-            text: {
-                value: detectStatus.toString(), //表示するテキスト
-                delimiter: "",  //区切り文字
-            },
-            ease: "ease",  // アニメーションのタイミング・進行割合を指定する
-        })
-    }
-
     useEffect(() => {
         init();
-        gsap.registerPlugin(TextPlugin);
-        setAnimation();
         const interval = setInterval(() => {
             getLocation();
         }, 1000);
@@ -116,7 +105,7 @@ export const GPSContent = () => {
     return (
         <Box>
             <Center pt="4" pb="8">
-                <Text color={detectColor} className="detect" fontFamily={"DotGothic16"} text={"5xl"} fontWeight={"bold"}></Text>
+                <Text color={detectColor} className="detect" fontFamily={"DotGothic16"} text={"5xl"} fontWeight={"bold"}>{detectStatus}</Text>
             </Center>
             <Flex w="full" gap="md">
                 <Button colorScheme={"secondary"} onClick={() => { sendMessage("LED ON"); }}>LEDを光らせる</Button>
