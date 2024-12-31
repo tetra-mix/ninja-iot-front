efc1a72f77ee080634919a0c68993052b9fce30d
tetra-mix <ryouma1128@outlook.jp>
2024-12-31 18:28:58 +0900

    upadate: pwa

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import env from "vite-plugin-env-compatible";
+import { VitePWA } from 'vite-plugin-pwa';
 import { defaultConfig, getColorModeScript } from "@yamada-ui/react";
 
 //プラグイン追加
@@ -24,5 +25,40 @@ export default defineConfig({
     react(),
     env({ prefix: "VITE", mountedPath: "process.env" }),
     injectScript(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      injectRegister: 'auto', //ここの記述
+      manifest: {
+        name: 'プレイヤー検知システム',
+        short_name: 'NinjaIOT', 
+        description: 'NinjaかくれんぼIOTの鬼側プレイヤー検知システム',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 });
